f3e67a47700e9d7f4f8a35905b6cfa4598207cb3
Simon Menke <simon.menke@gmail.com>
2017-01-25 18:34:00 +0100

    Improving the build script

--- test_context_go11.go
+++ test_context_go11.go
@@ -1,4 +1,4 @@
-// +build !go1.7
+// +build !go1.8
 
 package httpmiddlewarevet
 
@@ -7,6 +7,8 @@ import "net/http"
 var contextTest = &testCase{
 	Name: "Context",
 	Func: func(t test, client *http.Client, serve func(h http.Handler) string) {
+		// r.Context is present since go1.7 but r.Context.Done() doesn't work
+		// reliably.
 		t.Skip()
 	},
 }
