935818310f9432d8c2bef3847c9dee70ca1c7949
romainmenke <romainmenke@gmail.com>
2017-07-11 14:39:35 +0200

    fix ipfilter test

--- _thirdparty/github.com/jpillora/ipfilter/vet.go
+++ _thirdparty/github.com/jpillora/ipfilter/vet.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/fd/httpmiddlewarevet/testing"
 	"github.com/jpillora/ipfilter"
 )
 
+type logger struct{}
+
+func (l *logger) Printf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
 func main() {
+
 	testing.Run(
 		testing.Middleware{
 			Name: "Wrap",
 			Func: func(h http.Handler) http.Handler {
 				return ipfilter.Wrap(h, ipfilter.Options{
 					BlockedCountries: []string{"CN", "RU"},
+					Logger:           &logger{},
 				})
 			},
 		},
