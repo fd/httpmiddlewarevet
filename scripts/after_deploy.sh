7d6c3be58cfa32127905d559d72df226f88470d9
Romain Menke <romainmenke@gmail.com>
2018-11-12 20:10:15 +0100

    getting there

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-GIMME_OUTPUT="$(gimme 1.x | tee -a $HOME/.bashrc)"
+GIMME_OUTPUT="$(gimme 1.9.x | tee -a ${TRAVIS_HOME}/.bashrc)" && eval "$GIMME_OUTPUT"
+
+echo "$GIMME_OUTPUT"
+
 GO_VERSION=$(echo "$GIMME_OUTPUT" | awk '{ print $3 }')
+
+echo "$GO_VERSION"
+
 GO_VERSION=${GO_VERSION#"go"}
 
 echo "$GO_VERSION"
