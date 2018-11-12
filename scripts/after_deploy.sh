b9afd990f302499f47052a8f4777954fe9c0931f
Romain Menke <romainmenke@gmail.com>
2018-11-12 19:43:31 +0100

    improve after deploy step

---
+++ scripts/after_deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+GIMME_OUTPUT="$(gimme 1.x | tee -a $HOME/.bashrc)"
+GO_VERSION = echo $GIMME_OUTPUT | awk "{ print $3 }"
+
+echo $GO_VERSION
+
+curl -X GET "https://httpmiddlewarevet.herokuapp.com/webhook/travis?commit=${TRAVIS_COMMIT}&version=${GO_VERSION}&branch=${TRAVIS_BRANCH}"
