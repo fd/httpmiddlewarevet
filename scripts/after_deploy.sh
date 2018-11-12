d44ac37e27015b219d47a34325ab89b02e2496d2
Romain Menke <romainmenke@gmail.com>
2018-11-12 19:51:22 +0100

    fixes

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -6,6 +6,6 @@ GIMME_OUTPUT="$(gimme 1.x | tee -a $HOME/.bashrc)"
 GO_VERSION = echo $GIMME_OUTPUT | awk "{ print $3 }"
 GO_VERSION = ${GO_VERSION#"go"}
 
-echo $GO_VERSION
+echo "$GO_VERSION"
 
 curl -X GET "https://httpmiddlewarevet.herokuapp.com/webhook/travis?commit=${TRAVIS_COMMIT}&version=${GO_VERSION}&branch=${TRAVIS_BRANCH}"
